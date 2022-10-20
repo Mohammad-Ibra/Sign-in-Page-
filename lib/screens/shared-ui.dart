d3030f59e7acd1ec0a1e78914f583ae6bc10de43
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2022-10-20 12:31:03 +0300

    Add stateless widget

---
+++ lib/screens/shared-ui.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Shared extends StatelessWidget {
+  const Shared({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
